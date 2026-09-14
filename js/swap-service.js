ec3231f9d73ac5fa0778eeb33d603835899865f6
ILS <99141005+ils94@users.noreply.github.com>
2026-09-13 22:32:30 -0300

    Refactor simulate and execute functions for direction

--- js/swap-service.js
+++ js/swap-service.js
@@ -1,28 +1,39 @@
 import { DEPIX_HEX, BTC_HEX, POOL_ID, state } from "./config.js";
 
-export async function simulate(amtDepix) {
-  const amountIn = BigInt(Math.round(amtDepix * 1e8)).toString();
+const PAIRS = {
+  depixToBtc: { assetIn: DEPIX_HEX, assetOut: BTC_HEX },
+  btcToDepix: { assetIn: BTC_HEX, assetOut: DEPIX_HEX },
+};
+
+export async function simulate(direction, amt) {
+  const { assetIn, assetOut } = PAIRS[direction];
+
+  const amountIn = direction === "btcToDepix"
+  ? BigInt(Math.round(amt)).toString()
+  : BigInt(Math.round(amt * 1e8)).toString();
 
   const simulation = await state.client.simulateSwap({
     poolId: POOL_ID,
-    assetInAddress: DEPIX_HEX,
-    assetOutAddress: BTC_HEX,
+    assetInAddress: assetIn,
+    assetOutAddress: assetOut,
     amountIn,
   });
 
-  return { amountIn, satsOut: Number(simulation.amountOut) };
+  return { amountIn, amountOut: Number(simulation.amountOut), direction };
 }
 
-export async function execute({ amountIn, satsOut, slippagePct }) {
+export async function execute({ amountIn, amountOut, direction, slippagePct }) {
+  const { assetIn, assetOut } = PAIRS[direction];
+
   const maxSlippageBps = Math.round(slippagePct * 100);
   const minAmountOut = (
-    (BigInt(satsOut) * BigInt(10000 - maxSlippageBps)) / 10000n
+    (BigInt(amountOut) * BigInt(10000 - maxSlippageBps)) / 10000n
   ).toString();
 
   return state.client.executeSwap({
     poolId: POOL_ID,
-    assetInAddress: DEPIX_HEX,
-    assetOutAddress: BTC_HEX,
+    assetInAddress: assetIn,
+    assetOutAddress: assetOut,
     amountIn,
     maxSlippageBps,
     minAmountOut,
